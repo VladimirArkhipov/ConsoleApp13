667105486ca8659f96e9e7d990e6a38b04b5ce34
Viktor1Lukashev <lukashev-viktor2012@yandex.ru>
2023-12-05 15:50:01 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0;true; i++)
+            {
+                ConsoleKeyInfo key = Console.ReadKey(true);
+                Console.WriteLine((byte)key.KeyChar);
+            }
+        }
+    }
+}
